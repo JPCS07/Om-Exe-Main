b39460ae1bdf6a0be7633b92674fca484d02fc9a
JPCS07 <official.jpcs@gmail.com>
2021-12-16 04:24:09 -0800

    firebase config changed

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase';
 
 var firebaseApp = firebase.initializeApp({
     // Your firebase credentials
-    apiKey: "process.env.API_KEY",
-    authDomain: "process.env.AUTH_DOMAIN",
-    projectId: "process.env.PROJECT_ID",
-    storageBucket: "process.env.STORAGE_BUCKET",
-    messagingSenderId: "process.env.MESSAGING_SENDER_ID",
-    appId: "process.env.APP_ID",
-    measurementId: "process.env.MEASUREMENT_ID"
+    apiKey: "AIzaSyAiPiKOtaak-x0-w0Yh1_cKKV4xo_7V9xw",
+    authDomain: "om-executive-1ae73.firebaseapp.com",
+    projectId: "om-executive-1ae73",
+    storageBucket: "om-executive-1ae73.appspot.com",
+    messagingSenderId: "237440064102",
+    appId: "1:237440064102:web:8b52d3bb93241f1d97eba7",
+    measurementId: "G-VJ5W208B3V"
 });
 
 var db = firebaseApp.firestore();
